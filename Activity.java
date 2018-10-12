ee9195fcb7d3e501a06d2846e4b67ec681034a77
JonahAnderson73 <34152496+JonahAnderson73@users.noreply.github.com>
2018-10-11 22:41:01 -0700

    Add files via upload

--- Activity.java
+++ Activity.java
@@ -1,8 +1,27 @@
 
-public class Activity {
+import java.util.ArrayList;
 
-	public Activity() {
-		// TODO Auto-generated constructor stub
+public class Activity {
+	String activityName;
+	int duration;
+	ArrayList<String> dependencies;
+
+	public Activity(String activityName, int duration, ArrayList<String> dependencies) {
+		this.activityName = activityName;
+		this.duration = duration;
+		this.dependencies = dependencies;
+	}
+
+	public String getActivityName() {
+		return this.activityName;
+	}
+
+	public int getDuration() {
+		return this.duration;
+	}
+
+	public ArrayList<String> getDependencies() {
+		return this.dependencies;
 	}
 
 }
